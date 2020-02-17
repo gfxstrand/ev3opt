e06d4cd9facf1083cfcaef7d23ea55240e31bf33
Jason Ekstrand <jason@jlekstrand.net>
2020-02-17 16:36:16 -0600

    Implement std::fmt::Display for IR types

--- src/ir.rs
+++ src/ir.rs
@@ -20,6 +20,8 @@
  * IN THE SOFTWARE.
  */
 
+use std::fmt;
+
 mod opcodes;
 pub use opcodes::Opcode;
 
@@ -30,8 +32,78 @@ pub enum Parameter {
     String(Vec<u8>),
 }
 
+impl fmt::Display for Parameter {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Parameter::Local(i) => write!(f, "local+{}", i),
+            Parameter::Global(i) => write!(f, "global+{}", i),
+            Parameter::Constant(x) => write!(f, "{}", x),
+            Parameter::String(v) => {
+                match std::string::String::from_utf8(v.to_vec()) {
+                    Ok(s) => write!(f, "\"{}\"", s),
+                    Err(_) => write!(f, "<invalid UTF8 string>"),
+                }
+            },
+        }
+    }
+}
+
 pub struct Instruction {
+    pub ip: u32,
     pub op: Opcode,
     pub inputs: Vec<Parameter>,
     pub outputs: Vec<Parameter>,
+}
+
+impl fmt::Display for Instruction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:5}: {:20}", self.ip, self.op.to_str())?;
+        for param in self.inputs.iter() {
+            write!(f, " {:10}", param)?;
+        }
+        for param in self.outputs.iter() {
+            write!(f, " {:10}", param)?;
+        }
+        Ok(())
+    }
+}
+
+pub struct Object {
+    pub owner_id: u16,
+    pub trigger_count: u16,
+    pub local_bytes: u32,
+
+    pub instrs: Vec<Instruction>,
+}
+
+impl fmt::Display for Object {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "object {{\n")?;
+        write!(f, "    owner_id: {}\n", self.owner_id)?;
+        write!(f, "    trigger_count: {}\n", self.trigger_count)?;
+        write!(f, "    local_bytes: {}\n", self.local_bytes)?;
+        write!(f, "\n")?;
+        for instr in self.instrs.iter() {
+            write!(f, "    {}\n", instr)?;
+        }
+        write!(f, "}}")
+    }
+}
+
+pub struct Image {
+    pub version: u16,
+    pub global_bytes: u32,
+
+    pub objects: Vec<Object>,
+}
+
+impl fmt::Display for Image {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "version: {}\n", self.version)?;
+        write!(f, "global_bytes: {}\n", self.global_bytes)?;
+        for obj in self.objects.iter() {
+            write!(f, "\n{}", obj)?;
+        }
+        Ok(())
+    }
 }
